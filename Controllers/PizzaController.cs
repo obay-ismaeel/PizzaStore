5264fac5ed0c3345f1cdfb250f8c71beff17c314
Obay Ismaeel <obayhany@gmail.com>
2023-09-03 17:44:02 -0400

    Created Pizza Model & Controller With Get Methods

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaStore.Models;
+using PizzaStore.Services;
+
+namespace PizzaStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> Index() => PizzaService.GetAll();
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
