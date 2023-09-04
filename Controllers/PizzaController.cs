cf89a2e6051528e3177b059d9bc082b5064e4e17
Obay Ismaeel <obayhany@gmail.com>
2023-09-04 08:08:08 -0400

    Added Create, Update & Delete Methods

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -8,26 +8,53 @@ namespace PizzaStore.Controllers
     [Route("[controller]")]
     public class PizzaController : ControllerBase
     {
-        // GET all action
         [HttpGet]
         public ActionResult<List<Pizza>> Index() => PizzaService.GetAll();
 
-        // GET by Id action
         [HttpGet("{id}")]
         public ActionResult<Pizza> Get(int id)
         {
             var pizza = PizzaService.Get(id);
 
-            if(pizza == null)
+            if(pizza is null)
                 return NotFound();
 
             return pizza;
         }
 
-        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        }
 
-        // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
 
-        // DELETE action
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+               return NotFound();
+            
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
