3716f17b0b04a66ee34bc75637038449e4d22017
Obay Ismaeel <obayhany@gmail.com>
2023-09-15 17:43:05 -0400

    Added sqlite database to store app data

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using PizzaStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlite<PizzaContext>("Data Source=PizzaStore.db");
 
 var app = builder.Build();
 
---
+++ data/PizzaContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+namespace PizzaStore.Data
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options) { }
+
+        public DbSet<Pizza> Pizzas => Set<Pizza>();
+    }
+}
